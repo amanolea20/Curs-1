d1e189b9fc5952d38fca69bada1afa3428979b75
amanolea20 <92268937+amanolea20@users.noreply.github.com>
2022-11-23 00:56:10 +0200

    Add files via upload

---
+++ tema1.1/Program.cs
@@ -0,0 +1,35 @@
+﻿//1. Write a C# Sharp program that takes email and password as input (type string).
+//After 3 wrong attempts, user will be rejected.
+
+using System;
+
+namespace tema1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NumberAttempts = 3;
+            var tryAttempts = 0;
+            for (int i = 0; i < NumberAttempts; i++)
+            {
+                Console.WriteLine("insert your email");
+                string email = Console.ReadLine();
+                Console.WriteLine("insert your password");
+                string password = Console.ReadLine();
+                tryAttempts++;
+                if ((email == "email.log.com") && (password == "password.log"))
+                {
+                    Console.WriteLine("Login successfully!");
+                    break;
+                }
+                else if (tryAttempts == NumberAttempts)
+                {
+                    Console.WriteLine("Login failed");
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
