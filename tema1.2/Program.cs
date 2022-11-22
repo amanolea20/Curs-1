01658962ccaddb18f4fe46de8e63ad1487a093ba
amanolea20 <92268937+amanolea20@users.noreply.github.com>
2022-11-23 00:58:34 +0200

    Add files via upload

---
+++ tema1.2/Program.cs
@@ -0,0 +1,31 @@
+﻿//2.Write a C# Sharp program that takes a character as input and check the input (lowercase)
+//is a vowel, a digit, or any other symbol.
+
+using System;
+
+
+namespace tema1._2
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            Console.WriteLine("Insert a character: ");
+            char character = Convert.ToChar(Console.ReadLine());
+            if (character == vowels[0] || character == vowels[1]|| character == vowels[2] || character == vowels[3] || character == vowels[4])
+           // if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+            {
+                Console.WriteLine("This character is a lowercase vowel!");
+            }else if((character>='0') && (character <='9'))
+            {
+                Console.WriteLine("This character is a digit!");
+            }
+            else
+            {
+                Console.WriteLine("This is another symbol");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ tema1.3/Program.cs
@@ -0,0 +1,32 @@
+﻿//3.Write a C# Sharp program that takes two numbers as input and returns
+//true or false when both numbers are even or odd.
+using System;
+
+namespace tema1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool output = true;
+            Console.WriteLine("Insert the first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insert the second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (((a % 2 == 0) && (b % 2 == 0)) || ((a % 2 != 0) && (b % 2 != 0)))
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                output = false;
+                Console.WriteLine(output);
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
